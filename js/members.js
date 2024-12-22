9f52ecbd41553dfcfb4b44d03aa6dceaa758b586
Gabriel Casanova <con.casanovaproductions@outlook.com>
2024-12-21 22:00:08 -0300

    fade in and out do texto

--- js/members.js
+++ js/members.js
@@ -1,12 +1,45 @@
-const memberIcon = $('.member-icon')
+const memberText  = $('#member-text');
+const memberIcon  = $('.member-icon');
+const closeButton = $('#close-button');
 
+let text_displaying = false
 
 // Animação flip
 $('.member-icon').on('click', function() {
-    const clickedIcon = $(this); // Obter o ícone específico
+    // Obter o ícone específico e executar animação flip
+    const clickedIcon = $(this); 
     clickedIcon
         .addClass('animate__animated animate__flip')
         .one('animationend', function() {
-            clickedIcon.removeClass('animate__animated animate__bounceOutLeft'); // Resetar animação quando acabar.
+            clickedIcon.removeClass('animate__animated animate__flipInX'); // Resetar animação quando acabar.
         });
+
+    // Fade in do texto.
+    if (!text_displaying) { // Fade in da direita
+        memberText
+        .addClass('animate__animated animate__fadeInRight') 
+        .one('animationend', function() {
+            memberText.removeClass('animate__animated animate__fadeInRight hidden');
+        });
+
+        text_displaying = true
+
+    } else {
+        memberText
+        .addClass('animate__animated animate__fadeIn') 
+        .one('animationend', function() {
+            memberText.removeClass('animate__animated animate__fadeIn hidden'); 
+        });
+    }
+})
+
+// Animação de fechar o texto
+closeButton.on('click', function () {
+    memberText
+        .addClass('animate__animated animate__fadeOutRight hidden')
+        .one('animationend', function() {
+            memberText.removeClass('animate__animated animate__fadeOutRight');
+        });
+
+    text_displaying = false
 });
