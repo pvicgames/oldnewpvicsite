96108b116819feac358ba59861b251753ee2f5af
Gabriel Casanova <con.casanovaproductions@outlook.com>
2025-02-19 20:02:22 -0300

    updated about page

--- js/navbar.js
+++ js/navbar.js
@@ -120,6 +120,7 @@ function createLinks() {
         { icon: '🏠', label: 'home',    href: 'index.html', target: '' },
         //{ icon: '🕹️', label: 'games',   href: 'games.html', target: '' },
         //{ icon: '🖼️', label: 'gallery', href: 'https://www.instagram.com/pvicvg/', target: '_blank' },
+        { icon: '💭', label: 'about',   href: 'about.html', target: '' },
         { icon: '☎️', label: 'contact', href: 'contact.html', target: '' }
     ];
 
